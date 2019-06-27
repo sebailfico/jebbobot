ed91eda468ad8f8a524652cd0ba0cb9a26afc5a0
Sebastiano Pera <sebailfico@yahoo.it>
2019-06-27 23:53:35 +0200

    Testing voice message handling

--- index.js
+++ index.js
@@ -14,21 +14,49 @@ const Markup = require('telegraf/markup')
 //   Markup.callbackButton('Delete', 'delete')
 // ])
 
-
+let hasToSaveVoiceMessage = false;
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.telegram.getMe().then((botInfo) => {
-    bot.options.username = botInfo.username
-  })
+  bot.options.username = botInfo.username
+})
 
 bot.start((ctx) => ctx.reply('Hello'))
 bot.help((ctx) => ctx.reply('Help message'))
 // bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message))
-bot.hears(/.*vagina.*/i,  ({ reply }) => reply("dev'essere bella") );
-bot.hears(/.*piano.*/i,  ({ reply }) => reply("È grande Rocco!") );
+bot.hears(/.*vagina.*/i, ({ reply }) => reply("dev'essere bella"));
+bot.hears(/.*piano.*/i, ({ reply }) => reply("È grande Rocco!"));
 
+
+
+bot.hears(/.*pape.*/i, ({ reply }) => reply("https://www.youtube.com/watch?v=IRtQDoYJ4mU"));
+
+// COMMANDS
+bot.command('quit', (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id)
+
+  // Using shortcut
+  ctx.leaveChat()
+})
+
+bot.command('lasbo', (ctx) => {
+  // Explicit usage
+  hasToSaveVoiceMessage = true;
+
+  ctx.reply("Gimme da Sbò...");
+})
+
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.on('voice', (ctx) => {
+
+  ctx.telegram.replyWithVoice (ctx.message.chat.id, ctx.message.voice.file_id, {})
+})
 
 
 // bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
 // bot.action('delete', ({ deleteMessage }) => deleteMessage())
 bot.launch()
+
+
